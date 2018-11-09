d6ff928b2a284b2d393f1c9c599b7601851b18e2
jhonatan-01 <44886099+jhonatan-01@users.noreply.github.com>
2018-11-08 21:58:09 -0500

    nuevo

---
+++ app/src/main/java/com/example/isat_07/proyecto/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.isat_07.proyecto;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+    }
+}
