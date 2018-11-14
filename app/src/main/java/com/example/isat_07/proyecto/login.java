7e013d85617b82ba9d3f30683197a31183a2c8e2
jhonatan-01 <44886099+jhonatan-01@users.noreply.github.com>
2018-11-13 22:10:56 -0500

    13-11

--- app/src/main/java/com/example/isat_07/proyecto/login.java
+++ app/src/main/java/com/example/isat_07/proyecto/login.java
@@ -1,42 +1,34 @@
 package com.example.isat_07.proyecto;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
     //Metodo para el boton Productos
-    public void productos(View View) {
+    public void productos(View View){
         Intent productos = new Intent(this, productos.class);
         startActivity(productos);
     }
-
     //Metodo para el boton Contactenos
-    public void Contactenos(View View) {
+    public void Contactenos(View View){
         Intent contactenos = new Intent(this, contactenos.class);
         startActivity(contactenos);
     }
-
     //Metodo para el boton Informacion
-    public void Informacion(View View) {
+    public void Informacion(View View){
         Intent informacion = new Intent(this, informacion.class);
         startActivity(informacion);
     }
-
     //Metodo para el boton Login
-    public void Login(View View) {
+    public void Login(View View){
         Intent login = new Intent(this, login.class);
         startActivity(login);
     }
